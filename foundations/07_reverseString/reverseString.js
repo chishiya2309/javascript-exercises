d12314c5e8ad0e5abbf26acfcd8606466a02f0f8
chishiya2309 <lequanghung.work@gmail.com>
2026-07-21 21:34:26 +0700

    Finish ex 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let result = "";
+    let i = word.length - 1;
+    while (i >= 0) {
+        result += word[i];
+        i--;
+    }
+    return result;
 };
 
 // Do not edit below this line
