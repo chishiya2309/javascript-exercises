9509110d193aa0f2b0d312120f718bf63d3c1872
chishiya2309 <lequanghung.work@gmail.com>
2026-07-22 11:25:31 +0700

    Finish 13_palinfromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,4 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const cleanStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  
+  // 2. Dùng 2 con trỏ chạy từ 2 đầu vào giữa
+  let left = 0;
+  let right = cleanStr.length - 1;
+
+  while (left < right) {
+    if (cleanStr[left] !== cleanStr[right]) {
+      return false; // Phát hiện không đối xứng -> Dừng ngay
+    }
+    left++;
+    right--;
+  }
+
+  return true;
 
 };
 
