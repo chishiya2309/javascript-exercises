aa848d2aa3992ed8ecb8eb5dc0dcdf85c30850af
chishiya2309 <lequanghung.work@gmail.com>
2026-07-22 16:50:47 +0700

    Finish 14_bibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+let array = [];
+array[0] = 0;
+array[1] = 1;
+for (let i = 2; i <= 50; i++) {
+    array[i] = array[i - 1] + array[i - 2];
+}
 
+const fibonacci = function(seq) {
+    let res = Number(seq);
+    if (res < 0) {
+        return "OOPS";
+    }
+    return array[res];
 };
 
 // Do not edit below this line
