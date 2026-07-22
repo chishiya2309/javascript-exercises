7fea657e3619992f24fa8db83a5caab23b1172b1
chishiya2309 <lequanghung.work@gmail.com>
2026-07-22 10:57:52 +0700

    finish 11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fah) {
+  let res = (fah - 32) / 1.8;
+  return Number.isInteger(res) ? res : Number(res.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let res = (cel * 1.8) + 32;
+  return Number.isInteger(res) ? res : Number(res.toFixed(1));
 };
 
 // Do not edit below this line
