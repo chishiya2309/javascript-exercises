463af08cdccf3a93cc4c44b942c4f7cc125e1eb8
chishiya2309 <lequanghung.work@gmail.com>
2026-07-22 20:01:43 +0700

    Finish 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+function getAge(birthYear, deathYear) {
+    const curentYear = new Date().getFullYear();
+    const endYear = deathYear || curentYear;
 
+    return endYear - birthYear;
+}
+
+const findTheOldest = function(people) {
+    let oldest = people[0];
+
+    for (let i = 1; i < people.length; i++) {
+        const currentPersonAge = getAge(people[i].yearOfBirth, people[i].yearOfDeath);
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+
+        if (currentPersonAge > oldestAge) {
+            oldest = people[i];
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
